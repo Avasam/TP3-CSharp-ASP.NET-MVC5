fc4e8197bfdb949ddef8bbe043eeab93d802c844
Unknown <therazerdude@gmail.com>
2017-12-21 17:01:22 -0500

    ajout d'annotation required pour la validation client dans le model

--- TP3/Models/Livre.cs
+++ TP3/Models/Livre.cs
@@ -1,15 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TP3.Models
 {
     public class Livre
     {
+        [Required(ErrorMessage = "Un isbn et requis")]
         public string isbn { get; set; }
+        [Required(ErrorMessage = "Un auteur et requis")]
         public string author { get; set; }
+        [Required(ErrorMessage = "Un title et requis")]
         public string title { get; set; }
+        [Required(ErrorMessage = "Un nombre de pages et requis")]
         public int nbPages { get; set; }
+        [Required(ErrorMessage = "Une edition et requise")]
         public string edition { get; set; }
+        [Required(ErrorMessage = "Une annee et requise"), Range(-9999,9999)]
         public int year { get; set; }
+        [Required(ErrorMessage = "Une langue et requise")]
         public string language { get; set; }
+        [Required(ErrorMessage = "Une description et requise")]
         public string description { get; set; }
+        [Required(ErrorMessage = "Un ou des keywords sont requis")]
         public string keywords { get; set; }
 
         public Livre(string isbn, string author, string title, int nbPages, string edition, int year, string language, string description, string keywords)
--- TP3/Models/User.cs
+++ TP3/Models/User.cs
@@ -1,11 +1,17 @@
 
+using System.ComponentModel.DataAnnotations;
+
 namespace TP3.Models
 {
     public class User
     {
+        [Required(ErrorMessage = "Un username et requis")]
         public string Username { get; set; }
+        [Required(ErrorMessage = "Un mot de passe est requis")]
         public string Password { get; set; }
+        [Required(ErrorMessage = "Un email est requis")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Un role et requis")]
         public string Role { get; set; }
 
         public User(string username, string password, string email, string role="user") {
